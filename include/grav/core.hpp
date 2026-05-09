f3183df059fed34e6c081925ea6b199cbedadede
Patrick Blood <pblood66@gmail.com>
2026-05-08 20:43:23 -0600

    add dot, cross, and component product operations

--- include/grav/core.hpp
+++ include/grav/core.hpp
@@ -18,9 +18,22 @@ namespace grav
         grav::real squareMagnitude() const;
         void normalize();
 
-        void operator*=(const real& x);
-        Vec3 operator*(const real& x) const;
-    
+        void operator*=(const real &x);
+        Vec3 operator*(const real &x) const;
+        void operator+=(const Vec3 &other);
+        Vec3 operator+(const Vec3 &other) const;
+        void operator-=(const Vec3 &other);
+        Vec3 operator-(const Vec3 &other) const;
+        void addScaledVector(const Vec3 &other, real scale);
+        Vec3 componentProduct(const Vec3 &other) const;
+        void componentProductUpdate(const Vec3 &other);
+        real dot(const Vec3 &other);
+        // dot product operator overload
+        real operator*(const Vec3 &other);
+        Vec3 cross(const Vec3 &other) const;
+        // cross product operator overload
+        void operator%=(const Vec3 &other);
+        Vec3 operator%(const Vec3 &other) const;
     };
 }
 
--- src/engine/core.cpp
+++ src/engine/core.cpp
@@ -50,4 +50,69 @@ namespace grav
     {
         return Vec3(this->x * x, this->y * x, this->z * x);
     }
+    void Vec3::operator+=(const Vec3 &other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+    }
+    Vec3 Vec3::operator+(const Vec3 &other) const
+    {
+        return Vec3(x + other.x, y + other.y, z + other.z);
+    }
+    void Vec3::operator-=(const Vec3 &other)
+    {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+    }
+    Vec3 Vec3::operator-(const Vec3 &other) const
+    {
+        return Vec3(x - other.x, y - other.y, z - other.z);
+    }
+    void Vec3::addScaledVector(const Vec3 &other, real scale)
+    {
+        x += other.x * scale;
+        y += other.y * scale;
+        z += other.z * scale;
+    }
+    Vec3 Vec3::componentProduct(const Vec3 &other) const
+    {
+        return Vec3(x * other.x, y * other.y, z * other.z);
+    }
+    void Vec3::componentProductUpdate(const Vec3 &other)
+    {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+    }
+    real Vec3::dot(const Vec3 &other)
+    {
+        return x * other.x + y * other.y + z * other.z;
+    }
+    real Vec3::operator*(const Vec3 &other)
+    {
+        return x * other.x + y * other.y + z * other.z;
+    }
+    Vec3 Vec3::cross(const Vec3 &other) const
+    {
+        return Vec3(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z, 
+            x * other.y - y * other.x
+        );
+    }
+    void Vec3::operator%=(const Vec3 &other)
+    {
+        (*this) = cross(other);
+    }
+    Vec3 Vec3::operator%(const Vec3 &other) const
+    {
+        return Vec3(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z, 
+            x * other.y - y * other.x
+        );
+    }
 }
+ 
